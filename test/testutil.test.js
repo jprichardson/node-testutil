b632ea31d1d4a9fa05d7ba46ee2dc32aaebc5c63
JP Richardson <jprichardson@gmail.com>
2012-12-11 08:19:12 -0600

    Cleaned up tests a bit.

--- test/testutil.test.js
+++ test/testutil.test.js
@@ -5,10 +5,6 @@ var testutil = require('testutil')
 
 if (!fs.existsSync) fs.existsSync = path.existsSync
 
-log = function(s) {
-  return console.log(s);
-};
-
 endsWith = function(s, suffix) {
   var l;
   l = s.length - suffix.length;
@@ -17,7 +13,7 @@ endsWith = function(s, suffix) {
 
 describe('testutil', function() {
   describe('+ createTempDir()', function() {
-    return it('should create a directory in the OS temp directory', function() {
+    it('should create a directory in the OS temp directory', function() {
       var dirName, dirName2, oldFunc;
       oldFunc = Date.now;
       Date.now = function() {
@@ -30,16 +26,18 @@ describe('testutil', function() {
       dirName2 = testutil.createTempDir();
       T(dirName === dirName2);
       T(fs.existsSync(dirName));
-      return Date.now = oldFunc;
-    });
-  });
+      Date.now = oldFunc;
+    })
+  })
+
   describe('+ createBuffer()', function() {
-    return it('should create a buffer of size N with random data', function() {
+    it('should create a buffer of size N with random data', function() {
       var buf;
       buf = testutil.createBuffer(10);
-      return T(buf.length === 10);
-    });
-  });
+      T(buf.length === 10);
+    })
+  })
+
   describe('+ createFileWithData()', function() {
     it('should create a file specified at by file path filled with random data of size N', function() {
       var buf, filePath;
@@ -50,9 +48,10 @@ describe('testutil', function() {
       testutil.createFileWithData(filePath, 10);
       T(fs.existsSync(filePath));
       buf = fs.readFileSync(filePath);
-      return T(buf.length === 10);
-    });
-    return it('+ should return the file path of the file created()', function() {
+      T(buf.length === 10);
+    })
+    
+    it('+ should return the file path of the file created()', function() {
       var filePath, filePath2;
       filePath = path.join(path.tempdir(), Date.now() + '');
       if (fs.existsSync(filePath)) {
@@ -60,19 +59,21 @@ describe('testutil', function() {
       }
       filePath2 = testutil.createFileWithData(filePath, 10);
       return T(filePath2 === filePath);
-    });
-  });
+    })
+  })
+
   describe('+ fetchTestFiles()', function() {
-    return it('should return the test files in the specified directory', function(done) {
-      return testutil.fetchTestFiles('./', function(files) {
+    it('should return the test files in the specified directory', function(done) {
+      testutil.fetchTestFiles('./', function(files) {
         T(endsWith(files[0], 'testutil.test.js'));
-        return done();
-      });
-    });
-  });
-  return describe('+ createTestDir()', function() {
+        done();
+      })
+    })
+  })
+
+  describe('+ createTestDir()', function() {
     describe('> when it doesnt exist', function() {
-      return it('should create a test dir', function() {
+      it('should create a test dir', function() {
         var dir;
         dir = path.join(path.tempdir(), 'test-myapp');
         if (fs.existsSync(dir)) {
@@ -80,23 +81,25 @@ describe('testutil', function() {
         }
         F(fs.existsSync(dir));
         testutil.createTestDir('myapp');
-        return T(fs.existsSync(dir));
-      });
-    });
-    return describe('> when it exists', function() {
-      return it('should delete the dir and create it', function() {
-        var dir;
-        dir = path.join(path.tempdir(), 'test-myapp');
+        T(fs.existsSync(dir));
+      })
+    })
+
+    describe('> when it exists', function() {
+      it('should delete the dir and create it', function() {
+        var dir = path.join(path.tempdir(), 'test-myapp');
         rimraf.sync(dir);
         fs.mkdirsSync(dir);
+        
         fs.writeFileSync(path.join(dir, 'blah'), 'afafaf');
         T(fs.existsSync(dir));
+        
         testutil.createTestDir('myapp');
         T(fs.existsSync(dir));
-        return T(fs.readdirSync(dir).length === 0);
-      });
-    });
-  });
-});
+        T(fs.readdirSync(dir).length === 0);
+      })
+    })
+  })
+})
 
 
